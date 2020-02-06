da4ff52b28ce28a03342fc4c8907ffe63ee345b9
shaunlsy <shaunlowshuennyuh@hotmail.com>
2020-02-06 14:11:03 +0000

    fix list view

--- js/note-list-view.js
+++ js/note-list-view.js
@@ -1,31 +1,30 @@
 (function(exports) {
     let Note = function(text) {
-        this.text = text
-
+        this.text = text;
     };
     let NoteBook = function() {
-        this.storage = []
-    }; 
+        this.storage = [];
+    };
 
-    NoteBook.prototype.saveNotes = function(notes){ 
-        this.storage.push(notes) 
-    }
+    NoteBook.prototype.saveNotes = function(notes) {
+        this.storage.push(notes);
+    };
 
-    NoteBook.prototype.convertToHTML = function(){
+    NoteBook.prototype.convertToHTML = function() {
+        var array = ["<ul>"];
+        for (let i = 0; i < this.storage.length; i++) {
+            var a = this.storage[i];
+            array.push(`<li><div>${a}</div></li>`);
+        }
+        array.push("</ul>");
 
-     var array = ['<ul>']
-     for(let i = 0; i < this.storage.length; i++){
-      var a = this.storage[i]
-      array.push(`<li><div>${a}</div></li>`)
-    }
-    array.push('</ul>')
-    
-    return array.join("")
-  }
-  
-  Note.prototype.notes = function(){ return this.text }
-  
-  exports.Note = Note
-  exports.NoteBook = NoteBook
+        return array.join("");
+    };
 
+    Note.prototype.notes = function() {
+        return this.text;
+    };
+
+    exports.Note = Note;
+    exports.NoteBook = NoteBook;
 })(this);
